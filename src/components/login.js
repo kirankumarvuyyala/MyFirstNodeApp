47cb37409ed02cd4a6ecceb5cbaf116dae3b8db7
kirankumar.v <kirankumar.v@digitral.com>
2024-10-29 16:50:44 +0530

    Git Ignore

--- src/components/login.js
+++ src/components/login.js
@@ -7,40 +7,47 @@ const NodeCache = require('node-cache');
 const mycache=new NodeCache();
 login.post('/api/login', async (req, res) => {
     try {
-       const oauth= req.headers['x-imi-oauth'];
-        const { data} = req.body;
-     const rc4Value=  rc4Decrypt(data,oauth);
-     const originalData = JSON.parse(decodeURIComponent(atob(rc4Value)));
-     originalData=JSON.stringify(originalData);
-     console.log(originalData);
-     const AdminList=mycache.get("AdminList");
-     if(AdminList==null){
-        AdminList=  GetAdminList();
-     }
-     const UserList=AdminList.some(user=>user.uname== originalData.uname)
-      await mysql.connect(config);
-        const request = new mysql.Request();
-        request.input("MODE", mysql.VarChar, MODE);
-        request.input("MOBILENO", mysql.VarChar, originalData.uname);
-        request.input("USERID", mysql.VarChar, originalData.pwd);
-        request.input("ATTEMPTS", mysql.Int, ATTEMPTS);
-        request.output("RETVAL", mysql.Int);
+        const oauth = req.headers['x-imi-oauth'];
+        const { data } = req.body;
+        const rc4Value = rc4Decrypt(data, oauth);
+        const originalData = JSON.parse(decodeURIComponent(atob(rc4Value)));
+        originalData = JSON.stringify(originalData);
+        console.log(originalData);
+        const AdminList = mycache.get("AdminList");
+        if (AdminList == null) {
+            AdminList = GetAdminList();
+            console.log("getting data from db");
+        }
+        const UserList = AdminList.some(user => user.uname == originalData.uname)
+        if (UserList) {
 
-        const resp = await request.execute("UDP_OTP_ATTEMPTS_TEMP");
-        let results = resp.output.RETVAL
-        console.log('results '+results);
-        if (results > 0) {
-            return res.status(200).json({code:200, message: "Updated SuccessFully" });
-        } else {
-            return res.status(901).json({code:901, message: 'Error while updating data' });
+            await mysql.connect(config);
+            const request = new mysql.Request();
+            request.input("MODE", mysql.VarChar, MODE);
+            request.input("MOBILENO", mysql.VarChar, originalData.uname);
+            request.input("USERID", mysql.VarChar, originalData.pwd);
+            request.input("ATTEMPTS", mysql.Int, ATTEMPTS);
+            request.output("RETVAL", mysql.Int);
+
+            const resp = await request.execute("UDP_OTP_ATTEMPTS_TEMP");
+            let results = resp.output.RETVAL
+            console.log('results ' + results);
+            if (results > 0) {
+                return res.status(200).json({ code: 200, message: "Updated SuccessFully" });
+            } else {
+                return res.status(901).json({ code: 901, message: 'Error while updating data' });
+            }
+        }
+        else{
+            
         }
     }
     catch (err) {
         console.log("err " + err);
         res.status(1).send(err);
     }
-    finally{
-            await mysql.close();
+    finally {
+        await mysql.close();
     }
 
 });
